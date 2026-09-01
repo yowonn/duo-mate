16c7b312d2f62c851d3fde5273e3a39c48def12a
Diego Chinchilla <diegochinchilla007@gmail.com>
2026-09-01 12:44:14 -0600

    Ya ejecuta el modulo de factorizacion

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         ControlEstudiante ControlEstudiante1 = new ControlEstudiante();
         ControlEstudiante1.registroEstudiante();
+
+        ControlMenu ControlMenu1 = new ControlMenu();
+        ControlMenu1.elegirTema();
     }
 }
